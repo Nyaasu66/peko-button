dc0f7c2f9b8327e06a7cfd8e78c2b9fc4344829d
CI <CI@example.com>
2021-07-07 03:55:58 +0000

    deploy

---
+++ precache-manifest.8c1ffffd94896e7092d98c3e4772fb72.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e55901b5c7634f463bd34cded6e744",
+    "url": "/1.png"
+  },
+  {
+    "revision": "7059423b930c6f037879a11a4875c7d9",
+    "url": "/2.png"
+  },
+  {
+    "revision": "4d7b29ea1260fe366bb873a3d297ca47",
+    "url": "/3.png"
+  },
+  {
+    "revision": "8709b9de7f4f27755678",
+    "url": "/css/app.880f967d.css"
+  },
+  {
+    "revision": "d9d77dc07f5d7e54a3ae",
+    "url": "/css/chunk-vendors.2c8045b0.css"
+  },
+  {
+    "revision": "9299c99f0806bf87ffd52037b736b688",
+    "url": "/cursors/link-cur.png"
+  },
+  {
+    "revision": "78c942da65cb737eaae8f9ce8a56f615",
+    "url": "/cursors/normal-cur.png"
+  },
+  {
+    "revision": "1cb3e19441fe1b9ee44b4643215eedcb",
+    "url": "/cursors/text-cur.png"
+  },
+  {
+    "revision": "4609c2e237eb41451b71cd5d307106f6",
+    "url": "/cursors/上下.cur"
+  },
+  {
+    "revision": "ef7362470ebc4d6256be901835e36683",
+    "url": "/cursors/不可用.cur"
+  },
+  {
+    "revision": "ebf8aeac4b057a7c304eebde16319fa9",
+    "url": "/cursors/候选.ani"
+  },
+  {
+    "revision": "57ff51e66465f2fb293bbfdec8b8ad2a",
+    "url": "/cursors/十字.cur"
+  },
+  {
+    "revision": "6bbe7cfd03dba28f0cbf50803ef517dc",
+    "url": "/cursors/后台运行.ani"
+  },
+  {
+    "revision": "6b88a81bf2a6830d4d5d9b2dd96f025f",
+    "url": "/cursors/左上右下.cur"
+  },
+  {
+    "revision": "6540e9f098c6e4f11d1084f78584cb25",
+    "url": "/cursors/左下右上.cur"
+  },
+  {
+    "revision": "25e6a8facea6002ce5de5faa95041ab1",
+    "url": "/cursors/左右.cur"
+  },
+  {
+    "revision": "e76bf7a6221b501747b91dd1087445b4",
+    "url": "/cursors/帮助选择.cur"
+  },
+  {
+    "revision": "961141bd091fc7fdba9c70605dc1838f",
+    "url": "/cursors/帮助选择.png"
+  },
+  {
+    "revision": "66d093a890f0fc37219555f7a9ea2f84",
+    "url": "/cursors/忙.ani"
+  },
+  {
+    "revision": "2ec0a6099f7161e5d5ffb9297d73d4f2",
+    "url": "/cursors/手写.cur"
+  },
+  {
+    "revision": "d0c7f26fede1efe8430ffdb30acd6951",
+    "url": "/cursors/文本选择.cur"
+  },
+  {
+    "revision": "1c5e23e06c2ecbbfe4b53fea7a554cc8",
+    "url": "/cursors/正常选择.cur"
+  },
+  {
+    "revision": "2e008b57e0e67b8bda9145ab0d6ef335",
+    "url": "/cursors/精确选择.cur"
+  },
+  {
+    "revision": "7424b1668c5607e9d2b473f63840dc1a",
+    "url": "/cursors/链接选择.cur"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/fonts/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/fonts/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/fonts/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/fonts/materialdesignicons-webfont.dc85ceeb.woff2"
+  },
+  {
+    "revision": "169680e2a4b888af0dc69f54e9a77524",
+    "url": "/img/vercel/dark.svg"
+  },
+  {
+    "revision": "d674713425aabfe158fb5890ae8c3665",
+    "url": "/img/vercel/light.svg"
+  },
+  {
+    "revision": "33689a24c79a3d45af2bfabce91ea0d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8709b9de7f4f27755678",
+    "url": "/js/app.4df03feb.js"
+  },
+  {
+    "revision": "d9d77dc07f5d7e54a3ae",
+    "url": "/js/chunk-vendors.e09f5895.js"
+  },
+  {
+    "revision": "a3f32f873867478582290b038c6d1576",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "21e8d5386ddf2c6e0c64a463fad21857",
+    "url": "/voices/an笹/你们欺负我，明明我唱的那么认真.mp3"
+  },
+  {
+    "revision": "2f04ffcb1ceec20e1dfcea5632bd5c1b",
+    "url": "/voices/an笹/你这样会让我讨厌你的哦 你居然真下的了手 居然下手那么重.mp3"
+  },
+  {
+    "revision": "52cb34896cddf92cc7c101674f7879f8",
+    "url": "/voices/an笹/别打了 别打了.mp3"
+  },
+  {
+    "revision": "eac8f78fac389bdcf5bd3b0da65c89ed",
+    "url": "/voices/an笹/好痛啊 别打了.mp3"
+  },
+  {
+    "revision": "7fd8daa56f60ad142f72b541a1183876",
+    "url": "/voices/gachi/mua.mp3"
+  },
+  {
+    "revision": "a8d8644e29bed5b2d3f32b4c9bf70598",
+    "url": "/voices/gachi/初次见面 我叫笹木咲.mp3"
+  },
+  {
+    "revision": "97d0da0cebce123a61f4a74bf00d11a2",
+    "url": "/voices/gachi/呀哟呀哟.mp3"
+  },
+  {
+    "revision": "815deca632de609d9967383da198f80a",
+    "url": "/voices/gachi/来啦来啦.mp3"
+  },
+  {
+    "revision": "8d158b64a60a2632f3be7ad0998d9795",
+    "url": "/voices/gachi/谢谢呀哟.mp3"
+  }
+]);
